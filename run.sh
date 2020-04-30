1417bca3604bb9d835bbb90c2cc731251e66f4ba
Philippe Marschall <philippe.marschall@gmail.com>
2020-04-30 19:34:36 +0200

    increase heap size

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@
 JAR_FILE=$(dirname $0)/target/jfr-leaker-0.1.0-SNAPSHOT.jar
 
 # mvn package
-${JAVA_HOME}/bin/java -Xmx32m -Xms32m -XX:MetaspaceSize=32m -XX:MaxMetaspaceSize=32m -XX:+HeapDumpOnOutOfMemoryError -XX:+UseSerialGC -jar $JAR_FILE
+${JAVA_HOME}/bin/java -Xmx64m -Xms64m -XX:MetaspaceSize=64m -XX:MaxMetaspaceSize=64m -XX:+HeapDumpOnOutOfMemoryError -XX:+UseSerialGC -jar $JAR_FILE
 
